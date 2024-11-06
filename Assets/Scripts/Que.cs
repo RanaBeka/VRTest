45d1fa4afc2f98d0cf4ce9e94e36cfb1568e89f2
RanaBeka <217ramram@gmail.com>
2024-11-06 14:08:26 +0100

    miau

---
+++ Assets/Scripts/Que.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Que : MonoBehaviour
+{
+    /// <summary>
+    /// //https://www.youtube.com/watch?v=bwzkBvm2y_c
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
