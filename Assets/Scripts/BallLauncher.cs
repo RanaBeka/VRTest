a332d5ed7182050d6cc358f6581232d758ae7e63
RanaBeka <217ramram@gmail.com>
2024-12-18 14:25:23 +0100

    hgewc

--- Assets/Scripts/BallLauncher.cs
+++ Assets/Scripts/BallLauncher.cs
@@ -8,14 +8,16 @@ public class   BallLauncher : MonoBehaviour
 {
     [SerializeField] GameObject ball;
     [SerializeField] float disparo;
-
+      [SerializeField] float currentTime;
+    [SerializeField] float nextShootTime;
     [Header("Dirección shooter")]
     [SerializeField] float maxY;
     [SerializeField] float minY;
     [SerializeField] float maxX;
     [SerializeField] float minX;
+    BallMovement BallMovement;
 
-    static float _forceMultiplier = 5f;
+    static float _forceMultiplier = 100f;
     // Start is called before the first frame update
     public static float _forceMultiply
     {
@@ -23,8 +25,7 @@ public class   BallLauncher : MonoBehaviour
         set { _forceMultiplier = value; }
     }
 
-    float currentTime;
-    float nextShootTime;
+  
 
     static bool _canShoot = false;
 
@@ -55,23 +56,30 @@ public class   BallLauncher : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        currentTime += Time.deltaTime;
-        if(_canShoot && currentTime > nextShootTime)
+        if (currentTime<nextShootTime)
         {
+            currentTime += Time.deltaTime;
+        }
+      
+        if( currentTime > nextShootTime)
+        {
+            currentTime = 0;
             Shoot();
-            nextShootTime = currentTime + disparo;
+           
+
         }
     }
 
     private void Shoot()
     {
         var launcherAngle = GetRandomLauncherAngles();
-        transform.rotation = Quaternion.Euler(launcherAngle.x, launcherAngle.y, launcherAngle.z);
+        transform.rotation = Quaternion.Euler(launcherAngle.x, launcherAngle.y, 0);
 
         GameObject ballInstance = Instantiate(ball, transform.position, transform.rotation);
 
         ballInstance.GetComponent<Rigidbody>().AddForce(gameObject.transform.TransformDirection(Vector3.forward) * _forceMultiplier, ForceMode.Impulse);
-
+     
+        currentTime = 0;
     }
     Vector3  GetRandomLauncherAngles()
     {
@@ -79,7 +87,7 @@ public class   BallLauncher : MonoBehaviour
 
         rotationAngle.x = Random.Range(minX, maxX);
         rotationAngle.y = Random.Range(minY, maxY);
-        rotationAngle.z = transform.eulerAngles.z;
+        rotationAngle.z = 0f;
 
         return rotationAngle;
     }
---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour 
+{
+
+	// Singleton instance
+	public static BallMovement Instance;
+
+	// public variables
+	public Rigidbody ballPrefab;
+	public Transform target;
+
+	// private variables
+	int mouseCount = 0;
+	Vector3 delta;
+	Vector3 pos;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+
+		
+		
+
+		
+                   
+
+			
+	}
+
+	public void KickBall ()
+	{
+		Rigidbody rBody = Instantiate (ballPrefab, new Vector3 (0.0f, 0.35f,0), Quaternion.identity) as Rigidbody;
+		rBody.AddForce (target.forward * 2000);
+		rBody.AddForce (target.up * Random.Range (400, 481));
+
+		int _random = Random.Range (0, 3);
+		switch (_random)
+		{
+		case 0:
+			rBody.AddForce (target.right * Random.Range (100, 301));
+			break;
+			
+		case 1:
+			rBody.AddForce (-target.right * Random.Range (100, 301));
+			break;
+		}
+	}
+
+	void ResetCount ()
+	{
+		mouseCount = 0;
+	}
+}
